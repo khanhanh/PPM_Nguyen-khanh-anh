2c6c9e10345266e12e1a9c3bedb60c2adecd88de
khánh anh nguyễn <khanhanh0205@gmail.com>
2020-04-08 23:22:00 +0700

    c# cu phap can ban lam quen voi lap trinh huong doi tuong

---
+++ ConsoleApp1/ConsoleApp1/Rectangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Rectangle
+    {
+        double length;
+        double width;
+        public void Acceptdetails()
+        {
+            length = 4.5;
+            width = 3.5;
+        }
+
+   
+        public double GetArea()
+        {
+            return length * width;
+        }
+
+     
+        public void Display()
+        {
+            Console.WriteLine("Chieu dai: {0}", length);
+            Console.WriteLine("Chieu rong: {0}", width);
+            Console.WriteLine("Dien tich: {0}", GetArea());
+        }
+    }
+}
